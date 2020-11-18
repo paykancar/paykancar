8d3a857062569b77a11ed72da4a25a7b6b59fa14
Your Name <you@example.com>
2020-11-18 20:30:47 +0330

    paykancar

---
+++ Term 2/8-1.py
@@ -0,0 +1,12 @@
+
+username = input('whats your username?')
+
+password = input('whats your password?')
+
+if username == 'poulstar' and  password == '1234':
+
+    print("YOU CAN COME IN {:)}")
+
+
+else:
+    print("YOU CAN'T COME IN {:(}")
---
+++ Term 2/8-2.py
@@ -0,0 +1,14 @@
+# a = "ajisa"
+# a = a.upper()
+#     print(a)
+
+n = "24"
+print(n.isnumeric())
+print(n.isalpha())
+
+
+
+
+
+
+
