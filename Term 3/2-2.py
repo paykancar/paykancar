f5243a17e40f9d1ea9f7b6ad7fa2957edde80b62
Your Name <you@example.com>
2021-02-17 20:04:41 +0330

    paykancar

--- Term 3/2-2.py
+++ Term 3/2-2.py
@@ -1,5 +1,46 @@
 import tkinter as tk
 
+
+col={
+    "0":"black",
+    "1":"brown",
+    "2":"red",
+    "3":"orange",
+    "4":"yellow",
+    "5":"green",
+    "6":"blue",
+    "7":"purple",
+    "8":"gray",
+    "9":"white",
+    
+
+}
+def cal():
+    val = spin1.get()+ spin2.get()+ int(spin3.get())*"0"+" ohm"
+    label. config(text=val)
+
+
+
+
+
+    if spin1.get() =="0":
+        spin1.config(bg=col[spin1.get()], fg="white")
+    else:
+        spin1.config(bg=col[spin1.get()], fg="black")
+
+    if spin2.get() =="0":
+        spin2.config(bg=col[spin2.get()], fg="white")
+    else:
+        spin2.config(bg=col[spin2.get()], fg="black")
+
+    if spin3.get() =="0":
+        spin3.config(bg=col[spin3.get()], fg="white")
+    else:
+        spin3.config(bg=col[spin3.get()], fg="black")
+
+
+
+
 root = tk.Tk()
 spin_cnf  = {
     "from_":0,
@@ -8,6 +49,8 @@ spin_cnf  = {
     "width":5,
     "font":("TIMS",20)
 }
+label= tk.Label(root, text="", font=("times",20))
+label.pack()
 frame = tk.Frame(root)
 frame.pack()
 spin1 = tk.Spinbox(frame, cnf=spin_cnf)
@@ -16,6 +59,6 @@ spin2 = tk.Spinbox(frame, cnf=spin_cnf)
 spin2.pack(side=tk.LEFT)
 spin3 = tk.Spinbox(frame, cnf=spin_cnf)
 spin3.pack(side=tk.LEFT)
-b = tk.Button(root, text="Calculate", font=("times",20))
+b = tk.Button(root, text="Calculate", command=cal, font=("times",20))
 b.pack()
 root.mainloop()
