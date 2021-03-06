020c4676e6570678fb504e50feac39464aba5fdd
Your Name <you@example.com>
2021-03-06 16:29:13 +0330

    paykancar

--- Term 3/5-1.py
+++ Term 3/5-1.py
@@ -1,8 +1,21 @@
 import tkinter as tk
 import reg
+import tkinter.ttk as ttk
+def srch():
+    file = open("names.txt", "r")
+    top = tk.Toplevel()
+    top.geometry(f"{root.winfo_width()}x{root.winfo_height()}")
+    text = tk.Text(top)
+    text.grid(row=0, column=0)
+    for l in file:
+        if search.get() in l:
+            text.insert(tk.INSERT, l)
+
+
+
 
 def register():
-    reg.register((name.get() , last.get() , birth.get() , code.get())
+    reg.register(name.get() , last.get() , birth.get() , code.get())
 
 root = tk.Tk() 
 
@@ -25,5 +38,11 @@ frame.grid(row=5, column=1, columnspan=1)
 
 tk.Button(frame, text="Register", command=register).grid(row=1, column=0)
 tk.Button(frame, text="Cancel", command=root.destroy).grid(row=1, column=1)
+
+
+ttk.Label(root, text="Search").grid(row=9, column=0)
+search = tk.Entry(root) 
+search.grid(row=9, column=1)
+tk.Button(root, text="Search", command=srch).grid(row=10, column=1, columnspan=2)
 
 root.mainloop()
